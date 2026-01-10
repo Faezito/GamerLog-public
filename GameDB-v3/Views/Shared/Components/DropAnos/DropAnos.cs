ad2f04eb46ef126e40670431309883adef983675
Matheus Faé <matheusfae@gmail.com>
2026-01-10 15:46:45 -0300

    Drop de anos

---
+++ GameDB-v3/Views/Shared/Components/DropAnos/DropAnos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Z2.Services;
+
+namespace GameDB_v3.Views.Shared.Components.DropAnos
+{
+    public class DropAnos : ViewComponent
+    {
+        private readonly IRegistroJogoServicos _reg;
+        public DropAnos(IRegistroJogoServicos reg)
+        {
+            _reg = reg;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int usuarioId)
+        {
+            var lst = await _reg.ListarAnos(usuarioId);
+            return View("Default", lst);
+        }
+    }
+}
