96816aa4c75f8b1dc3bf4ba2989fa071e34cbfe3
Matheus Faé <matheusfae@gmail.com>
2026-03-08 18:15:31 -0300

    Classe para manipular datas

---
+++ Z4.Lib/DateTimeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z4.Lib
+{
+    public static class DateTimeExtensions
+    {
+        public static string ObterMesCompleto(this int m)
+        {
+            return m switch
+            {
+                1 => "Janeiro",
+                2 => "Fevereiro",
+                3 => "Março",
+                4 => "Abril",
+                5 => "Maio",
+                6 => "Junho",
+                7 => "Julho",
+                8 => "Agosto",
+                9 => "Setembro",
+                10 => "Outubro",
+                11 => "Novembro",
+                12 => "Dezembro",
+                _ => ""
+            };
+        }
+    }
+}
