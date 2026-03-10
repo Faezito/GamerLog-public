2ccbbfb50b337e9071f741b8be878f4a4b779747
Faezito <matheusfae@gmail.com>
2026-03-10 09:19:58 -0300

    View component separado para card Rawg

---
+++ GameDB-v3/Views/Shared/Components/CardRawg/CardRawg.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Z1.Model;
+using Z1.Model.APIs;
+
+namespace GameDB_v3.Views.Shared.Components.CardJogo
+{
+    public class CardRawg : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(RawgGameDto model)
+        {
+            return View("Default", model);
+        }
+    }
+}
