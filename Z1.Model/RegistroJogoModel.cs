7e57855c63cddc3869f693c04f31dd901313c62f
Matheus Faé <matheusfae@gmail.com>
2026-03-30 14:02:46 -0300

    Validações com Data Annotation

--- Z1.Model/RegistroJogoModel.cs
+++ Z1.Model/RegistroJogoModel.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Z1.Model.APIs;
+﻿using System.ComponentModel.DataAnnotations;
 using Z1.Model.Lang;
 
 namespace Z1.Model
 {
     public class RegistroJogoModel : JogoModel
     {
-        public int UsuarioID { get; set; }
-        public int JogoID { get; set; }
+        public int? UsuarioID { get; set; }
+        public int? JogoID { get; set; }
         public int PlataformaID { get; set; }
         public string? Plataforma { get; set; }
         public decimal TempoJogado {  get; set; }
+
+        [Required(ErrorMessage = "Insira o tempo jogado, pode ser aproximado.")]
         public string? TempoJogadoString {  get; set; }
         public decimal? TempoJogadoTotal {  get; set; }
         public DateTime DataAdicionado { get; set; }
         public DateTime? UltimaSessao { get; set; }
         public DateTime? DataPlatinado { get; set; }
         public DateTime? DataZerado { get; set; }
+
+        [Required(ErrorMessage = "Insira uma data.")]
         public DateTime? DataInput { get; set; }
         public int? Nota {  get; set; }
         public int Status { get; set; } // 0 - abandonado, 1 - jogando, 2 - finalizado, 3 - 100%, 9 - platinado
